72c28bb6ecf107dfff7413a96518d7123043ff85
JAntunes22 <jpc.antunes22@gmail.com>
2020-10-05 22:29:12 +0100

    ola

--- source/ricochet_robots.py
+++ source/ricochet_robots.py
@@ -7,94 +7,128 @@
 # 00000 Nome2
 
 from search import Problem, Node, astar_search, breadth_first_tree_search, \
-    depth_first_tree_search, greedy_search
+	depth_first_tree_search, greedy_search
 import sys
 
 
 class RRState:
-    state_id = 0
+	state_id = 0
 
-    def __init__(self, board):
-        self.board = board
-        self.id = RRState.state_id
-        RRState.state_id += 1
+	def __init__(self, board):
+		self.board = board
+		self.id = RRState.state_id
+		RRState.state_id += 1
 
-    def __lt__(self, other):
-    	""" Este método é utilizado em caso de empate na gestão da lista
-        de abertos nas procuras informadas. """
-        return self.id < other.id
+	def __lt__(self, other):
+		""" Este método é utilizado em caso de empate na gestão da lista
+		de abertos nas procuras informadas. """
+		return self.id < other.id
 
+class Robot:
+
+	def __init__(self, color, x, y):
+		self.color = color
+		self.x = x
+		self.y = y
+
+	def getPos():
+		return (self.x, self.y)
+	
+	def getColor():
+		return self.color
 
 class Board:
-    """ Representacao interna de um tabuleiro de Ricochet Robots. """
+	""" Representacao interna de um tabuleiro de Ricochet Robots. """
 
-    def robot_position(self, robot: str):
-        """ Devolve a posição atual do robô passado como argumento. """
-        # TODO
-        pass
+	def __init__(self, robots, target, barriers):
+		self.robots = robots
+		self.target = target
 
-    # TODO: outros metodos da classe
+	def robot_position(self, robot: str):
+		""" Devolve a posição atual do robô passado como argumento. """
+		for r in self.robots:
+			if r.getColor() == robot:
+				return r.getPos()
+		
+		pass
+
+	# TODO: outros metodos da classe
 
 
 def parse_instance(filename: str) -> Board:
-    """ Lê o ficheiro cujo caminho é passado como argumento e retorna
-    uma instância da classe Board. """
-    # TODO
-    pass
+	""" Lê o ficheiro cujo caminho é passado como argumento e retorna
+	uma instância da classe Board. """
+	# TODO
+
+	robots = []
+	barriers = []
+
+	file = open(filename, "r")
+
+	n = int(file.readline())
+	for i in range(0, n):
+		input_str = file.readline()
+		split_str = input_str.split()
+		robot = Robot(split_str[0], int(split_str[1]), int(split_str[2]))
+		robots.append(robot)
+
+	input_str = file.readline()
+	target = input_str.split()
+
+	n = int(file.readline())
+	for i in range(0, n):
+		input_str = file.readline()
+		split_str = input_str.split()
+		robot = Robot(split_str[0], int(split_str[1]), int(split_str[2]))
+		barriers.append(input_str)
+
+	return Board(robots, target, barriers)
+
+
+	pass
 
 
 class RicochetRobots(Problem):
-    def __init__(self, board: Board):
-        """ O construtor especifica o estado inicial. """
-        # TODO: self.initial = ...
-        pass
+	def __init__(self, board: Board):
+		""" O construtor especifica o estado inicial. """
+		# TODO: self.initial = ...
+		pass
 
-    def actions(self, state: RRState):
-        """ Retorna uma lista de ações que podem ser executadas a
-        partir do estado passado como argumento. """
-        # TODO
-        pass
+	def actions(self, state: RRState):
+		""" Retorna uma lista de ações que podem ser executadas a
+		partir do estado passado como argumento. """
+		# TODO
+		pass
 
-    def result(self, state: RRState, action):
-        """ Retorna o estado resultante de executar a 'action' sobre
-        'state' passado como argumento. A ação retornada deve ser uma
-        das presentes na lista obtida pela execução de
-        self.actions(state). """
-        # TODO
-        pass
+	def result(self, state: RRState, action):
+		""" Retorna o estado resultante de executar a 'action' sobre
+		'state' passado como argumento. A ação retornada deve ser uma
+		das presentes na lista obtida pela execução de
+		self.actions(state). """
+		# TODO
+		pass
 
-    def goal_test(self, state: RRState):
-        """ Retorna True se e só se o estado passado como argumento é
-        um estado objetivo. Deve verificar se o alvo e o robô da
-        mesma cor ocupam a mesma célula no tabuleiro. """
-        # TODO
-        pass
+	def goal_test(self, state: RRState):
+		""" Retorna True se e só se o estado passado como argumento é
+		um estado objetivo. Deve verificar se o alvo e o robô da
+		mesma cor ocupam a mesma célula no tabuleiro. """
+		# TODO
+		pass
 
-    def h(self, node: Node):
-        """ Função heuristica utilizada para a procura A*. """
-        # TODO
-        pass
+	def h(self, node: Node):
+		""" Função heuristica utilizada para a procura A*. """
+		# TODO
+		pass
 
 
 if __name__ == "__main__":
-    # TODO:
-    # Ler o ficheiro de input de sys.argv[1],
-    # Usar uma técnica de procura para resolver a instância,
-    # Retirar a solução a partir do nó resultante,
-    # Imprimir para o standard output no formato indicado.
-    pass
+	# TODO:
+	# Ler o ficheiro de input de sys.argv[1],
+	# Usar uma técnica de procura para resolver a instância,
+	# Retirar a solução a partir do nó resultante,
+	# Imprimir para o standard output no formato indicado.
 
+	board = parse_instance(sys.argv[1])
 
-def readInput(robots, target, barriers):
-    n = input()
-    for i in range(0, n):
-        input_str = input()
-        pos = input_str.split()
-        robots.append(pos)
-    input_str = input()
-    target = input_str.split()
-    n = input()
-    for i in range(0, n):
-        input_str = input()
-        pos = input_str.split()
-        barriers.append(pos)
+	
+	pass
